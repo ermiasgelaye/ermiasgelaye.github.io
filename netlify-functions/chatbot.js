b8efca7ca452b737534b8e7fc2c12f1bf7c14fd6
ermiasgelaye <ermiasgelaye@gmail.com>
2023-11-19 21:28:02 -0500

    comit
    commit

--- netlify-functions/chatbot.js
+++ netlify-functions/chatbot.js
@@ -1,9 +1,17 @@
-// netlify-functions/chatbot.js
-exports.handler = async function (event, context) {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello, I am your chatbot!' }),
-  };
+// chatbot.js
+document.addEventListener('DOMContentLoaded', function () {
+  const chatContainer = document.getElementById('chatContainer');
+  const chatbotButton = document.getElementById('chatbotButton');
 
-  return response;
-};
+  chatbotButton.addEventListener('click', function () {
+    // Replace this with your chatbot logic or API calls
+    const message = 'Hello, I am your chatbot!';
+    displayMessage(message);
+  });
+
+  function displayMessage(message) {
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    chatContainer.appendChild(messageElement);
+  }
+});
